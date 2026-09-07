445adab450ab1cfc898f314125e93c62a6e3828a
nethmi-wanigasooriya <nethmiwanigasooriya878@gmail.com>
2026-09-07 15:14:36 +0530

    Change next.config to standard JS format

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -3,10 +3,4 @@ const nextConfig = {
   /* config options here */
 };
 
-export default nextConfig;import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
-};
-
-export default nextConfig;
+module.exports = nextConfig;
